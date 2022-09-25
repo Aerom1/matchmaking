a719c0241d78b94ca2b9702fee1ac56cc067c172
Aerom1 <romaindelon@gmail.com>
2022-09-25 21:50:22 +0200

    repasse index en html

--- bonjour.php
+++ bonjour.php
@@ -3,6 +3,35 @@
   <title>Test PHP</title>
  </head>
  <body>
- <?php echo '<p>Bonjour le monde</p>'; ?>
+
+<?php 
+    echo '<p>Bonjour le mondeuuhka 192.168.1.40</p>'; 
+    
+    // phpinfo();
+
+    echo phpinfo();
+    
+    $servername = "192.168.1.40";
+    $username = "root";
+    $password = "";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+    // Check connection
+    if (!$conn) {
+        die("Connection mysqli_connect procedural : failed. Error: " . mysqli_connect_error());
+    }
+    echo "Connected successfully with mysqli_connect procedural";
+
+    $db = "dbplayer";
+
+    $conn = new mysqli($servername, $username, $password,$db) or die("Connect MySQLi Object-Oriented failed: %s\n". $conn -> error);
+
+    echo "\n\n -> FIN DES TESTS DE CONNEXION";
+
+
+
+
+ ?>
  </body>
 </html>
